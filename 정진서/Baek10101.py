8ab96ef8203187f46e8b1d48390d1366fe6c5c55
Bmony <129152378+Bmony@users.noreply.github.com>
2023-07-05 00:28:39 +0900

    정진서
    5073, 9063, 10101
    삼각형과 세 변
    대지
    삼각형 외우기

---
+++ 정진서/Baek10101.py
@@ -0,0 +1,12 @@
+## 삼각형 외우기
+a = int(input())
+b = int(input())
+c = int(input())
+if a == 60 and b == 60 and c == 60:
+    print('Equilateral')
+elif a + b + c == 180 and a != b and b != c and c != a:
+    print('Scalene')
+elif a + b + c != 180:
+    print('Error')
+else:
+    print('Isosceles')
